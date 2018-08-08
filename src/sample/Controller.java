d03ec4c286198e49235a57d98627fa6661792120
D070715 <mazen010>
2018-08-09 01:26:31 +0200

    CNC

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,12 @@
 package sample;
 
+import javafx.fxml.FXML;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+
 public class Controller{
+
+
+
 }
