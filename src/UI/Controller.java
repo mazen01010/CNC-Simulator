8e67e2affd6520039540794cab0ebcc47828d571
D070715 <mazen010>
2018-08-09 03:54:42 +0200

    CNC

--- src/UI/Controller.java
+++ src/UI/Controller.java
@@ -1,5 +1,7 @@
 package UI;
 
+import javafx.geometry.Point2D;
+import javafx.geometry.VPos;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.Button;
@@ -7,6 +9,7 @@ import javafx.scene.control.Label;
 import javafx.scene.*;
 import javafx.scene.paint.*;
 import javafx.scene.canvas.*;
+import javafx.scene.text.TextAlignment;
 
 public class Controller{
 
@@ -14,12 +17,21 @@ public class Controller{
     public Button Start;
     public Button Pause;
     public Button Stop;
-    public Label ComandLabel;
+    public Label CommandLabel;
     public Canvas canvas;
 
     public void initialize(){
-        GraphicsContext g = canvas.getGraphicsContext2D();
-        g.setFill(Color.GRAY);
-        g.fillRect(0, 0, canvas.getWidth(), canvas.getHeight());
+        GraphicsContext gc = canvas.getGraphicsContext2D();
+        gc.setFill(Color.GRAY);
+        gc.fillRect(0, 0, canvas.getWidth(), canvas.getHeight());
+
+        gc.setTextAlign(TextAlignment.CENTER);
+        gc.setTextBaseline(VPos.CENTER);
+        gc.setStroke(Color.GREEN);
+        gc.strokeText("+",canvas.getWidth()/2,canvas.getHeight()/2);
+
+
     }
+
+
 }
